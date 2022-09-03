386e7c5035cdb1c4c60569979949897455856989
Chaeyoung02 <chaedud02@naver.com>
2022-09-04 04:06:58 +0900

    bootstrap

---
+++ bootstrap/js/local.js
@@ -0,0 +1,18 @@
+function LoginPage(){
+    window.open("/signin/form_signin.html");
+}
+function productPage(){
+    window.location.href="/product/product_page.html";
+}
+function product(){
+    window.location.href="/product/product.html";
+}
+
+function mainPage(){
+    window.location.href="/main.html";
+}
+
+
+function back(){
+   history.back();
+}
